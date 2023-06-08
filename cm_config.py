c8a10fe325705910711ec710b7df3a6e3ff3c72f
Bór Milán <milanbor40@gmail.com>
2023-06-08 09:26:03 +0200

    feat(logging): added logging and a lil fix

--- cm_config.py
+++ cm_config.py
@@ -1,7 +1,40 @@
-PRODUCTION = True
+import logging
+
+PRODUCTION = False
 
 IMAGE_PATH = "images"
 
 DETECTOR_CONFIG = {
     "test" : "test"
 }
+
+LOG_PATH = (
+    ""
+    if PRODUCTION
+    else "api_log.log" 
+)
+
+_logger = logging.Logger("cm-logger", level=logging.DEBUG)
+_fh = logging.FileHandler(LOG_PATH)
+_fh.setFormatter(
+    logging.Formatter(
+        fmt="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+)
+_fh.setLevel(logging.INFO)
+_logger.addHandler(_fh)
+
+
+class Logger:
+    def debug(msg: str):
+        _logger.debug(msg)
+
+    def info(msg: str):
+        _logger.info(msg)
+
+    def warning(msg: str):
+        _logger.warning(msg)
+
+    def error(msg: str):
+        _logger.error(msg)
