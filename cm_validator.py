3f3fe6579935fb736c063eddd11e9874fe724494
Bór Milán <milanbor40@gmail.com>
2023-08-07 11:07:23 +0200

    refactor(log_data): new structure

--- cm_validator.py
+++ cm_validator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from cm_config import Logger
 from startup import mongo
 from datetime import datetime
 from sklearn.linear_model import LinearRegression
@@ -14,6 +15,7 @@ def validate(detector, new_value, threshold=0.2):
     try:
         new_value = int(new_value)
     except ValueError:
+        Logger.info("int(new_value)")
         return False
 
     current_time = round(time.time() * 1000)
@@ -25,9 +27,11 @@ def validate(detector, new_value, threshold=0.2):
         y.append(log["value"])
 
     if len(X) < 2:  # Can not do much with that
+        Logger.info("len < 2")
         return True
 
     if new_value < y[-1]:
+        Logger.info("new_value < y[-1]")
         return False
 
     X = np.array(X)
@@ -41,4 +45,6 @@ def validate(detector, new_value, threshold=0.2):
     average = (new_value + predicted_value) / 2
     percent_diff = (diff / average) * 100
 
-    return True if percent_diff <= threshold else False
+    Logger.info("regr")
+    # return True if percent_diff <= threshold else False
+    return True
