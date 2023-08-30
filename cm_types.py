4697798633577733855e0daec00588920492001a
Bór Milán <milanbor40@gmail.com>
2023-08-30 15:16:52 +0200

    feat(token-renew): renew on every success response

--- cm_types.py
+++ cm_types.py
@@ -1,15 +1,9 @@
-from cm_config import Logger
-
+from cm_config import Logger, JWT_COOKIE_KEY, SESSION_PERSISTANCE_TIME
+from flask import make_response, request
 
 def error_response(endpoint: str, data):
     # Logger.error(f"error response - endpoint: {endpoint} - data: {data}")
     return {"result": "error", "data": data}
-
-
-def success_response(endpoint: str, data):
-    # Logger.info(f"success response - endpoint: {endpoint} - data: {data}")
-    return {"result": "ok", "data": data}
-
 
 def user_data(creation_time: str, name: str, email: str, password_salt: str, password_hash: str, token: str):
     return {
@@ -21,3 +15,18 @@ def user_data(creation_time: str, name: str, email: str, password_salt: str, pas
         "email_verification_token": token,
         "config": {},
     }
+
+def success_response(endpoint, data):
+    token = request.cookies.get(JWT_COOKIE_KEY)
+
+    response = make_response({"result": "ok", "data": data})
+
+    response.set_cookie(
+        key=JWT_COOKIE_KEY,
+        value=token,
+        max_age= SESSION_PERSISTANCE_TIME,
+        secure=True,
+        samesite="None"
+    )
+
+    return response
