c332429f78055eda9724c9dc9ffba921e4ffb7d9
Bór Milán <milanbor40@gmail.com>
2023-09-25 12:57:05 +0200

    fix(lineplot,polars): lineplot with polars data processing

--- api/diagrams/locaion_monthly_pie.py
+++ api/diagrams/locaion_monthly_pie.py
@@ -1,45 +1,42 @@
 from startup import app, mongo
-from api.api_utils import success_response 
-from domain.detector import Detector
+from api.api_utils import success_response, error_response 
 from cm_config import TYPE_COLORS
 from api import login_required
 
-from flask import abort
-from datetime import datetime
-import numpy as np
-import pandas as pd
+import polars as pl
 
 @app.route("/get_location_pie/<location_id>", methods=["GET"])
 @login_required
 def get_location_pie(_, location_id):
-    detectors_raw = mongo.detectors.find({"location_id": location_id})
-    detectors = [Detector(detector_row) for detector_row in detectors_raw]
+    logs_raw = list(mongo.logs.find({"location_id": location_id}))
+    if logs_raw == []:
+        return error_response("no log found")
+    
+    return success_response(prepare_piechart_data(logs_raw))
 
-    return success_response(prepare_piechart_data(detectors))
+def prepare_piechart_data(logs: list[dict]):
+    pie_data = {}
 
-def prepare_piechart_data(detectors: list[Detector]):
-    # pie_data = {}
-    #
-    # current_month = datetime.now().month
-    # for detector in detectors:
-    #     monthly_cost = detector.consumption_by_month(current_month) * detector.detector_config.cost
-    #     if detector.type in pie_data.keys():
-    #         pie_data[detector.type] = pie_data[detector.type] + \
-    #             float(monthly_cost)
-    #     else:
-    #         pie_data[detector.type] = float(
-    #            monthly_cost) 
-    #
-    # reformatted_data = []
-    # for key in pie_data.keys():
-    #     reformatted_data.append(
-    #         {
-    #             "id": key,
-    #             "label": key,
-    #             "value": np.round(pie_data[key], 3),
-    #             "color": TYPE_COLORS[key]
-    #         }
-    #     )
-    #
-    # return reformatted_data
-    return None
+    df = pl.from_dicts(logs)
+
+    df = df.with_columns((df['value'] - df['value'].shift()).alias("consumption"))
+
+    grouped = df.group_by('type').agg(pl.col('consumption').sum())
+    print(grouped)
+
+    reformatted_data = []
+    for type in grouped.rows():
+        name = type[0]
+        value = type[1]
+        reformatted_data.append(
+            {
+                "id": name,
+                "label": value,
+                "value": round(value, 3),
+                "color": TYPE_COLORS[name]
+            }
+        )
+            
+    return reformatted_data
+
+
