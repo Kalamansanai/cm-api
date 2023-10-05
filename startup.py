56d4471be17fda1520c8be18a01b8901bbe54653
Bálint Dániel <balint.daniel623@gmail.com>
2023-10-05 08:56:29 +0200

    fix(Image save path)

--- startup.py
+++ startup.py
@@ -1,12 +1,15 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from pymongo import MongoClient
 
 import certifi
 
-from cm_config import DB_NAME
+from cm_config import MONGO_URI, MODE, DB_NAME, IMAGE_PATH
 
-from cm_config import MONGO_URI, MODE
+if(not os.path.exists(os.path.join(IMAGE_PATH, "images"))):
+     os.mkdir(os.path.join(IMAGE_PATH, "images"))
 
 
 app = Flask(__name__)
