d35feec8ec39ec5b712ca6aeaf172dc7644df082
Bór Milán <milanbor40@gmail.com>
2023-10-05 10:29:25 +0200

    change(pie data processing): separated the reformatting

--- api/diagrams/locaion_monthly_pie.py
+++ api/diagrams/locaion_monthly_pie.py
@@ -1,10 +1,11 @@
 from startup import app, mongo
-from api.api_utils import success_response, error_response 
+from api.api_utils import success_response, error_response
 from cm_config import TYPE_COLORS
 from api import login_required
 
 from datetime import datetime
 import polars as pl
+
 
 @app.route("/get_location_pie/<location_id>", methods=["GET"])
 @login_required
@@ -12,8 +13,12 @@ def get_location_pie(_, location_id):
     logs_raw = list(mongo.logs.find({"location_id": location_id}))
     if logs_raw == []:
         return error_response("no log found")
-    
-    return success_response(prepare_piechart_data(logs_raw))
+
+    table = prepare_piechart_data(logs_raw)
+    dict = table_to_dict(table)
+
+    return success_response(dict)
+
 
 def prepare_piechart_data(logs: list[dict]):
     df = pl.from_dicts(logs)
@@ -23,16 +28,21 @@ def prepare_piechart_data(logs: list[dict]):
     df = df.sort("type")
 
     df = df.with_columns(
-            pl.when((df["timestamp"].dt.month() == current_month) & (df["type"].shift() == df["type"]))
-            .then((df['value'] - df['value'].shift()))
-            .otherwise(0)
-            .alias("consumption")
+        pl.when(
+            (df["timestamp"].dt.month() == current_month)
+            & (df["type"].shift() == df["type"])
         )
+        .then((df["value"] - df["value"].shift()))
+        .otherwise(0)
+        .alias("consumption")
+    )
 
-    grouped = df.group_by('type').agg(pl.col('consumption').sum())
+    return df.group_by("type").agg(pl.col("consumption").sum())
 
+
+def table_to_dict(table):
     reformatted_data = []
-    for type in grouped.rows():
+    for type in table.rows():
         name = type[0]
         value = type[1]
         reformatted_data.append(
@@ -40,10 +50,8 @@ def prepare_piechart_data(logs: list[dict]):
                 "id": name,
                 "label": value,
                 "value": round(value, 3),
-                "color": TYPE_COLORS[name]
+                "color": TYPE_COLORS[name],
             }
         )
-            
+
     return reformatted_data
-
-
